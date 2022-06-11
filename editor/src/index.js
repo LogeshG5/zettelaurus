2dfb6bff5e0a467636775ed64fe64b18f5c11014
Logesh G <logeshg5@gmail.com>
2022-06-11 14:13:06 +0530

    Avoid multiple componentDidMount trigger by removing StrictMode

--- editor/src/index.js
+++ editor/src/index.js
@@ -5,8 +5,6 @@ import ToastEditor from './ToastEditor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ToastEditor />
-  </React.StrictMode>
+  <ToastEditor />
 );
 
